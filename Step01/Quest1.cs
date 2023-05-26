9d8c12dd599bbca29ad592761936475aa953fa15
nam2626 <nam2626@naver.com>
2023-05-26 14:50:35 +0900

    문제

--- Step01/Quest1.cs
+++ Step01/Quest1.cs
@@ -12,6 +12,14 @@ namespace Step01
          * 직사각형의 가로길이, 세로길이 입력을 받아서
          * 입력한 직사각형의 넓이를 계산해서 출력
          */
+        static void Main(string[] args)
+        {
+            Console.Write("가로 길이 입력 : ");
+            int width = int.Parse(Console.ReadLine());
+            Console.Write("세로 길이 입력 : ");
+            int height = int.Parse(Console.ReadLine());
 
+            Console.WriteLine(width * height);
+        }
     }
 }
