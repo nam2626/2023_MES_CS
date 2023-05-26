d990717b7a36f53480fbdfaacaffb3cc6e714669
nam2626 <nam2626@naver.com>
2023-05-26 14:22:38 +0900

    변수

---
+++ Step01/Variable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step01
+{
+    internal class Variable
+    {
+        static void Main(string[] args)
+        {
+            bool flag = true;
+            Console.WriteLine(flag);
+
+            int n1 = 1000;
+            //unsigned int 
+            uint n2 = 2000;
+            Console.WriteLine($"n1 = {n1}, n2 = {n2}");
+
+            uint n3 = uint.MaxValue;
+            int n4 = int.MaxValue;
+            Console.WriteLine($"n3 = {n3}, n4 = {n4}");
+
+            string str = "Hello World";
+            Console.WriteLine(str);
+
+            var a = 10;
+            var b = "HelloWorld";
+            var c = 3.1415;
+            var d = true;
+            var e = 'C';
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+        }
+    }
+}
