852e3b5ea3e6b52134e6e321b697612f90f31f98
nam2626 <nam2626@naver.com>
2023-05-26 16:16:00 +0900

    메서드

---
+++ Step01/Method.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step01
+{
+    internal class Method
+    {
+        public static int sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        public static int mul(int n1=10, int n2=20)
+        {
+            return n1 * n2;
+        }
+
+        static void Swap(int n1, int n2)
+        {
+            int temp = n1;
+            n1 = n2;
+            n2 = temp;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(sum(1, 2));
+            Console.WriteLine(mul());
+            Console.WriteLine(mul(30));
+            Console.WriteLine(mul(30,40));
+
+            int n1 = 10;
+            int n2 = 20;
+
+            Swap(n1, n2);
+            Console.WriteLine(n1 + " " + n2);
+
+        }
+    }
+}
