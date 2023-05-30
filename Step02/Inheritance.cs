1f0de84119ed941164b49b4e6303bed5eec86c72
nam2626 <nam2626@naver.com>
2023-05-30 11:22:37 +0900

    상속 예제

--- Step02/Inheritance.cs
+++ Step02/Inheritance.cs
@@ -17,10 +17,24 @@ namespace Step02
 
         public int Age { get => age; set => age = value; }
 
-        public void printInfo()
+        public virtual void printInfo()
         {
             Console.WriteLine($"이 동물의 나이 : {Age}");
         }
+    }
+
+    class Person : Animal
+    {
+        public Person(int age) : base(age)
+        {
+        }
+
+        public override void printInfo()
+        {
+            Console.WriteLine($"이 사람의 나이{age}");
+        }
+
+
     }
     internal class Inheritance
     {
