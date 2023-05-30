5001df0d3fc425488dd7522cbb2dca4090f48383
nam2626 <nam2626@naver.com>
2023-05-30 09:38:55 +0900

    클래스 구성요소 자동완성

---
+++ Step02/Class_Ex.cs
@@ -0,0 +1,57 @@
+﻿namespace Step02
+{
+    class Animal
+    {
+        private string name;
+        private int age;
+
+        //생성자
+        public Animal()
+        {
+            Console.WriteLine("기본 생성자");
+        }
+
+        public Animal(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        //프로퍼티(Property)
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Animal animal &&
+                   name == animal.name &&
+                   age == animal.age;
+        }
+
+        //정보 출력하는 메서드
+        public void printAnimalInfo()
+        {
+            Console.WriteLine($"이 동물의 이름 : {Name}, 나이 : {Age}");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a = new Animal();
+            a.Age = 20;
+            a.Name = "개";
+            a.printAnimalInfo();
+
+        }
+
+    }
+}
