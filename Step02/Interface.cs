0df91f5ca1784e44d6303bfd646dac5c18c1cc4f
nam2626 <nam2626@naver.com>
2023-05-30 12:27:29 +0900

    인터페이스

---
+++ Step02/Interface.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step02
+{
+    interface Power
+    {
+        public readonly static bool POWER_ON = true;
+        public static readonly bool POWER_OFF = false;
+        public abstract void PowerOn();
+        public void PowerOff();
+    }
+    class TV : Power
+    {
+        public void PowerOff()
+        {
+            Console.WriteLine("TV 전원 Off");
+        }
+
+        public void PowerOn()
+        {
+            Console.WriteLine("TV 전원 On");
+        }
+    }
+    internal class Interface
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
