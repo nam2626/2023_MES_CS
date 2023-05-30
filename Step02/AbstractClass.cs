42298f0a5210b039333a5c570ea71dac3c7fbfbb
nam2626 <nam2626@naver.com>
2023-05-30 12:13:09 +0900

    상속, 추상클래스

---
+++ Step02/AbstractClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step02
+{
+    //추상 클래스, 추상 메서드
+    abstract class Gun
+    {
+        public abstract void shot();
+        public void reload()
+        {
+            Console.WriteLine("Reload!!!");
+        }
+    }
+
+    class M16 : Gun
+    {
+        public override void shot()
+        {
+            Console.WriteLine("Bang!!");
+        }
+    }
+
+    internal class AbstractClass
+    {
+        static void Main(string[] args)
+        {
+            //추상 클래스는 직접적으로 생성 X
+            //Gun a = new Gun();
+            M16 m16 = new M16();
+            m16.reload();
+            m16.shot();
+        }
+    }
+}
+
+
+
--- Step02/Inheritance.cs
+++ Step02/Inheritance.cs
@@ -16,6 +16,12 @@ namespace Step02
             Console.WriteLine("Animal 생성자");
         }
 
+        ~Animal()
+        {
+            //소멸자 호출은 자식부터 먼저 소멸되고 부모가 소멸
+            Console.WriteLine("Animal 소멸자 호출");
+        }
+
         public int Age { get => age; set => age = value; }
 
         public virtual void printInfo()
@@ -30,7 +36,10 @@ namespace Step02
         {
             Console.WriteLine("Person 생성자");
         }
-
+        ~Person()
+        {
+            Console.WriteLine("Person 소멸자");
+        }
         public override void printInfo()
         {
             base.printInfo();//부모가 가지고 있는 printInfo 실행
@@ -41,12 +50,22 @@ namespace Step02
     }
     internal class Inheritance
     {
+        static void test()
+        {
+            Person p = new Person(1);
+        }
         static void Main(string[] args)
         {
             Person p = new Person(100);
             p.printInfo();
             Animal a = p;
             a.printInfo();
+
+            Animal b = new Animal(100);
+            b.printInfo();
+            test();
+            GC.Collect();
+            Console.ReadLine();
         }
     }
 }
