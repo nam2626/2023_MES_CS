46fc5f986623641e7b88e06b134b1ef2b02a5a1c
nam2626 <nam2626@naver.com>
2023-05-30 11:12:21 +0900

    상속

---
+++ Step02/Inheritance.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step02
+{
+    class Animal
+    {
+        protected int age;
+
+        public Animal(int age)
+        {
+            this.age = age;
+        }
+
+        public int Age { get => age; set => age = value; }
+
+        public void printInfo()
+        {
+            Console.WriteLine($"이 동물의 나이 : {Age}");
+        }
+    }
+    internal class Inheritance
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
