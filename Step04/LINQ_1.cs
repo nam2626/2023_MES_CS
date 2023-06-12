6b9eac2c1791ebb1dd7ec4f04dc4e7c4df096080
nam2626 <nam2626@naver.com>
2023-06-12 11:29:09 +0900

    LINQ

--- Step04/LINQ_1.cs
+++ Step04/LINQ_1.cs
@@ -9,6 +9,7 @@ namespace Step04
     internal class LINQ_1
     {
         /* select 추출할 데이터
+         * orderby 정렬기준
          * where 조건절
          * from 데이터 집합
          */
@@ -17,6 +18,7 @@ namespace Step04
             int[] numbers = { 9, 2, 6, 4, 5, 3, 7, 8, 1, 10 };
 
             var result = from n in numbers
+                         orderby n descending
                          where n % 2 == 0
                          select n;
 
---
+++ Step04/LINQ_2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Step04
+{
+    class Profile
+    {
+        private string name;
+        private int height;
+        public string Name { get => name; set => name = value; }
+        public int Height { get => height; set => height = value; }
+    }
+    internal class LINQ_2
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
